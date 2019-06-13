a5552f30fb6763f6851b23b792d71b734c050691
jasper qiu <13928412827@163.com>
2019-06-13 16:02:24 +0800

    添加简历配置文件及小程序结构更改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,19 @@
 /** index.js **/
-
+const config = require('../../resume.config.js');
+console.log("dfdf---",config);
 Page({
   data: {
     blog: 'https://www.cnblogs.com/AnotherLife/',
     phone: '13928412827',
     mail: '13928412827@163.com',
     github: 'https://github.com/qiujianping-work',
-    demo:""
+    demo: "",
+    technologies: config.technologies,
+    tools: config.tools,
+    experiences: config.experiences,
+    introduce: config.introduce,
   },
-
+  //博客按钮
   copyBlog() {
     wx.setClipboardData({
       data: this.data.blog,
@@ -22,35 +27,23 @@ Page({
     })
   },
 
-  copyDemo() {
-    wx.setClipboardData({
-      data: this.data.demo,
-      success() {
-        wx.showToast({
-          title: '内容已复制',
-          icon: "none",
-          duration: 1000
-        })
-      }
-    })
-  },
-
+  //电话按钮
   actionSheetTap() {
+    const phoneNum = this.data.phone;
     wx.showActionSheet({
       itemList: ['呼叫', '复制'],
       success(e) {
-        console.log(e.tapIndex)
-        if (e.tapIndex===0){
-            wx.makePhoneCall({
-              phoneNumber: '13824339381',
-              success() {
-                console.log('成功拨打电话')
-              }
-            })
+        if (e.tapIndex === 0) {
+          wx.makePhoneCall({
+            phoneNumber: phoneNum,
+            success() {
+              console.log('成功拨打电话')
+            }
+          })
         }
-        else if (e.tapIndex === 1){
+        else if (e.tapIndex === 1) {
           wx.setClipboardData({
-            data: '13824339381',
+            data: phoneNum,
             success() {
               wx.showToast({
                 title: '内容已复制',
@@ -63,7 +56,7 @@ Page({
       }
     })
   },
-
+  //邮件按钮
   copyMail() {
     wx.setClipboardData({
       data: this.data.mail,
@@ -76,7 +69,7 @@ Page({
       }
     })
   },
-
+  //github按钮
   copyGithub() {
     wx.setClipboardData({
       data: this.data.github,
---
+++ resume.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  introduce: "本人从事前端开发工作4年，主要技术栈为jQuery、react全家桶、微信小程序等，有过大型React项目开发经验，熟悉webpack、gulp等前端自动化构建工具。曾主导过项目开发，有带领团队开发产品经验，热爱前端开发，注重团队协作。",
+  technologies:[
+    "4年HTML、CSS、JavaScript经验，熟悉W3C标准规范",
+    "4年JQuery、2年React框架经验, 理解MVC、MVVM模式",
+    "1年node.js、express、mysql经验，具有服务端开发经验",
+    "1年echarts.js, highcharts.js经验，善于数据可视化开发",
+    "熟悉promise对象、set和map数据结构、async函数、class类 等ECMAScript新语法",
+    "熟悉ajax，axios等http请求技术，灵活运用于浏览器及node端",
+    "熟悉ant design、layUI等UI库，能熟练应用到项目中实现快速开发",
+    "熟练进行微信小程序开发",
+    "熟练使用前端CSS集成工具SASS、LESS，减少重复性操作，提高开发效率",
+    "熟练掌握前端性能优化，有效的对代码进行精简提升前端性能",
+    "熟练使用Linux常用命令，对nginx有一定了解"
+  ],
+  tools:[
+    "3年xshell、winscp等服务端工具使用经验",
+    "熟练使用webpack、gulp前端自动化构建工具及Git版本管理工具",
+    "熟练使用VSCode, Sublime等主流IDE进行开发工作"
+  ],
+  experiences: [
+    {
+      companyName: "深圳云天励飞技术有限公司",
+      position: "前端开发高级工程师",
+      dateRanges: "2016.09-2019.05",
+      explain: "任职于深圳云天励飞技术有限公司软件部，担任前端开发高级工程师一职，负责web前端开发工作。该公司是一家人工智能技术创新型企业，公司产品主要以智能安防领域为主，其中本人参与的项目“云天深目”系统，曾协助公安机关破获多起重要案件。另外参与项目“行人闯红灯”系统现已在深圳多处试点使用，并获得相关新闻报道。在该公司任职期间，牵头重构“云天深目”系统，推动前端组件化、业务模块化。"
+    },
+    {
+      companyName: "深圳市英趣科技有限公司",
+      position: "web前端开发工程师",
+      dateRanges: "2015.03-2016.08 ",
+      explain: "深圳英趣科技有限公司是一家互联网解决方案供应商，在该公司本人任职于研发部，负责web前端开发工作，具体工作内容包含产品需求评审、web页面构建、UI交互实现、后端数据呈现等。"
+    },
+    {
+      companyName: "深圳亚太卫视文化传播有限公司",
+      position: "运维技术员",
+      dateRanges: "2014.06-2015.03 ",
+      explain: "负责公司网络、服务器等运维工作"
+    }
+  ]
+}
